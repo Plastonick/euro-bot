e6934d26f268b35fe454755eb159422759800bc6
David Pugh <plastonick@gmail.com>
2022-11-08 19:26:58 +0000

    log before sending

--- src/Loop.php
+++ src/Loop.php
@@ -31,32 +31,32 @@ class Loop
 
             // Determine if the match has started since our last status
             if ($originalMatch->status === 'SCHEDULED' && $updatedMatch->status !== 'SCHEDULED') {
-                $this->messager->matchStarting($updatedMatch);
                 $this->logger->debug('Generating match start event');
+                $this->messager->matchStarting($updatedMatch);
             }
 
             // Check for goals scored
             if ($updatedMatch->homeScore > $originalMatch->homeScore) {
-                $this->messager->goalScored($updatedMatch->homeTeam, $updatedMatch);
                 $this->logger->debug('Generating home team goal scored event');
+                $this->messager->goalScored($updatedMatch->homeTeam, $updatedMatch);
             }
 
             if ($updatedMatch->homeScore < $originalMatch->homeScore
                 || $updatedMatch->awayScore < $originalMatch->awayScore
             ) {
-                $this->messager->goalDisallowed($updatedMatch);
                 $this->logger->debug('Generating goal disallowed event');
+                $this->messager->goalDisallowed($updatedMatch);
             }
 
             if ($updatedMatch->awayScore > $originalMatch->awayScore) {
-                $this->messager->goalScored($updatedMatch->awayTeam, $updatedMatch);
                 $this->logger->debug('Generating away team goal scored event');
+                $this->messager->goalScored($updatedMatch->awayTeam, $updatedMatch);
             }
 
             // Determine if the match has finished
             if ($originalMatch->status !== 'FINISHED' && $updatedMatch->status === 'FINISHED') {
-                $this->messager->matchComplete($updatedMatch);
                 $this->logger->debug('Generating match completion event');
+                $this->messager->matchComplete($updatedMatch);
             }
         }
 
